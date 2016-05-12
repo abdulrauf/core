bcb51c4f778c17cca148296c6226b059e65d62a9
Abdul Rauf <abdul.rauf.au@gmail.com>
2016-05-12 21:05:54 +0930

    Listing pages - WIP
    Signed-off-by: Abdul Rauf <abdul.rauf.au@gmail.com>

--- app/assets/javascripts/gluttonberg.js
+++ app/assets/javascripts/gluttonberg.js
@@ -10,8 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require ./jquery-1-10-1
-//= require ./gb_bootstrap
+//= require ./jquery-2-2-3-min
+//= require ./gb-bootstrap-min
 //= require ./jquery-ui-1-10-3.custom-min
 //= require ./chosen-jquery-min
 //= require ./jquery-validate
@@ -23,4 +23,7 @@
 //= require ./jcrop/js/jquery-Jcrop-min
 //= require ./gb_soundmanager2-jsmin
 //= require ./gb_360player
+
+//= require ./gentelella/custom
+
 //= require ./gb_application
--- app/helpers/gluttonberg/admin.rb
+++ app/helpers/gluttonberg/admin.rb
@@ -28,7 +28,7 @@ module Gluttonberg
 
       # Writes out a link styled like a button. To be used in the sub nav only
       def nav_link(*args)
-        class_names = "button"
+        class_names = ""
         class_names = "#{class_names} #{args[2][:class]}" if args.length >= 3
         content_tag(:li, active_link_to(args[0] , args[1] , :title => args[0]), :class => class_names)
       end
